fb386bcd4a591dcefb151519353bdb1cbed01096
sureframe846 <57026394+sureframe846@users.noreply.github.com>
2019-12-14 16:57:05 -0500

    Save to  sqlite db and csv

--- scrapeKenpom.py
+++ scrapeKenpom.py
@@ -1,14 +1,41 @@
-
-# coding: utf-8
-
-# In[225]:
-
-
 import numpy as np
 import pandas as pandas
 import requests
+import sqlite3
+from datetime import date
+
 from bs4 import BeautifulSoup
 
+conn = sqlite3.connect('kenpom.db')
+c = conn.cursor()
+sqlcode = """
+CREATE TABLE If NOT EXISTS STATS( 
+    Rank int,
+    team varchar(22),
+    conf text,
+    `w-l` text,
+    adjEM real,
+    adjO real,
+    adjO_seed int,
+    adjD real,
+    adjD_seed int,
+    adjT real,
+    adjT_seed int,
+    luck real,
+    luck_seed int,
+    sched real,
+    sched_seed int,
+    OppO real,
+    OppO_seed int,
+    OppD real,
+    OppD_seed int,
+    ncadjEM real,
+    ncadjem_seed int,
+    date date,
+    PRIMARY KEY(team, date)
+)
+"""
+c.execute(sqlcode)
 
 def getArray(url): 
     page = requests.get(url)
@@ -26,8 +53,11 @@ def getArray(url):
     
 
 def makeDF(data):
-    columns = ['Rk','Team','Conf','W-L','AdjEM','AdjO','AdjO seed','AdjD','AdjD seed','AdjT','AdjT seed','Luck','Luck seed','AdjEM','AdjEM seed','OppO', 'OppO seed','OppD','OppD seed','AdjEM','AdjEM seed']
+    currentDate = date.today()
+    columns = ['Rank','team','conf','w-l','adjEM','adjO','adjO_seed','adjD','adjD_seed','adjT','adjT_seed','luck','luck_seed','sched','sched_seed','OppO', 'OppO_seed','OppD','OppD_seed','ncadjEM','ncadjEM_seed']
     df = pandas.DataFrame(data, columns = columns)
+    datevec = pandas.DataFrame([{'date': currentDate}] * len(df)) 
+    df = df.join(datevec, how = 'left')
     return df
     
     
@@ -35,4 +65,5 @@ def makeDF(data):
 games2020 = getArray("https://kenpom.com/index.php")
 mydf = makeDF(games2020)
 print(mydf)
-
+mydf.to_sql('STATS', conn, if_exists = 'append', index = False)
+mydf.to_csv('kenpom.csv', mode = 'a', header = False, index = False, date_format = str)
