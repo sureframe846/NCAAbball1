8c3e3e4d0ecf2b4f06cbd020773c96b8de66c179
sureframe846 <57026394+sureframe846@users.noreply.github.com>
2021-03-18 23:05:27 -0400

    run for 2021

--- scrapeKenpom.py
+++ scrapeKenpom.py
@@ -1,3 +1,9 @@
+
+# coding: utf-8
+
+# In[225]:
+
+
 import numpy as np
 import pandas as pandas
 import requests
@@ -5,6 +11,29 @@ import sqlite3
 from datetime import date
 
 from bs4 import BeautifulSoup
+
+def getArray(url): 
+    page = requests.get(url)
+    y = []
+    soup1 = BeautifulSoup(page.content, 'html.parser')
+    for tr in soup1.find_all('tr'):
+        for td in tr.find_all('td'):
+    
+            y.append(td.get_text())
+        
+    games = np.array(y).reshape(int(len(y)/21),21)
+    #print(games)
+    return(games)
+    
+
+def makeDF(data):
+    currentDate = date.today()
+    columns = ['Rank','team','conf','w-l','adjEM','adjO','adjO_seed','adjD','adjD_seed','adjT','adjT_seed','luck','luck_seed','sched','sched_seed','OppO', 'OppO_seed','OppD','OppD_seed','ncadjEM','ncadjEM_seed']
+    df = pandas.DataFrame(data, columns = columns)
+    datevec = pandas.DataFrame([{'date': currentDate}] * len(df)) 
+    df = df.join(datevec, how = 'left')
+    return df
+
 
 conn = sqlite3.connect('kenpom.db')
 c = conn.cursor()
@@ -35,35 +64,10 @@ CREATE TABLE If NOT EXISTS STATS(
     PRIMARY KEY(team, date)
 )
 """
-c.execute(sqlcode)
-
-def getArray(url): 
-    page = requests.get(url)
-    y = []
-    soup1 = BeautifulSoup(page.content, 'html.parser')
-    for tr in soup1.find_all('tr'):
-        for td in tr.find_all('td'):
-    
-            y.append(td.get_text())
-
-
-    games = np.array(y).reshape(int(len(y)/21),21)
-    #print(games)
-    return(games)
-    
-
-def makeDF(data):
-    currentDate = date.today()
-    columns = ['Rank','team','conf','w-l','adjEM','adjO','adjO_seed','adjD','adjD_seed','adjT','adjT_seed','luck','luck_seed','sched','sched_seed','OppO', 'OppO_seed','OppD','OppD_seed','ncadjEM','ncadjEM_seed']
-    df = pandas.DataFrame(data, columns = columns)
-    datevec = pandas.DataFrame([{'date': currentDate}] * len(df)) 
-    df = df.join(datevec, how = 'left')
-    return df
-    
-    
+#c.execute(sqlcode)    
     
 games2020 = getArray("https://kenpom.com/index.php")
 mydf = makeDF(games2020)
 print(mydf)
 mydf.to_sql('STATS', conn, if_exists = 'append', index = False)
-mydf.to_csv('kenpom.csv', mode = 'a', header = False, index = False, date_format = str)
+mydf.to_csv('kenpom.csv', mode = 'a', header = False, index = False)
